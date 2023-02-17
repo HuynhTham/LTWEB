936c1c06e717c978f95c94d58b200be557ee235b
taki123s <20130305@st.hcmuaf.edu.vn>
2023-02-17 12:34:54 +0700

    Start project

---
+++ AnimeWeb/src/main/java/controller/begin.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import database.Login;
+import database.blog;
+import database.movie;
+import model.ListAccount;
+import model.ListBlog;
+import model.ListMovie;
+
+
+@WebServlet("/anime-main/begin")
+public class begin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+			movie daoMovie = new movie();
+			blog daoBlog = new blog();   
+			ListMovie listMovie;
+			ListBlog listBlog;
+			ListAccount listAccount;
+			
+			try {
+				listMovie = daoMovie.getMovie();
+				listBlog = new ListBlog(daoBlog.getlistBlog());
+				listAccount = new ListAccount(new Login().getConnection());
+				getServletContext().setAttribute("listMovie", listMovie);
+				getServletContext().setAttribute("listBlog", listBlog);
+				getServletContext().setAttribute("listUser", listAccount);
+			
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				response.getWriter().println("<img class=\"rsImg\" src=\"/AnimeWeb/error.png"+"\">");
+				//e.printStackTrace();
+				
+			}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+	}
+
+}
